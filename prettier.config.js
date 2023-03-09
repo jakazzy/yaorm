e36ed7d67c486bc8b9c8dde4f17a3ef9a65e085a
Yaw Boakye <wheresyaw@gmail.com>
2023-03-09 23:49:37 +0000

    add a little bit of setup
    in this changeset:
    - add typescript, initialize a tsconfig
    - add eslint (for linting)
    - add jest (for testing)
    - add prettier (for standardizing code format)
    
    and a few other things that make them work with typescript.

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	trailingComma: "es5",
+	tabWidth: 4,
+	semi: true,
+	singleQuote: false,
+	printWidth: 90,
+	bracketSpacing: false,
+	arrowParens: "avoid",
+	useTabs: true,
+	plugins: ["prettier-plugin-organize-imports"],
+};
