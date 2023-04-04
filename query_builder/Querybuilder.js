e90c8f20250387619b28f28e149ba94274067773
Anthony4m <amoahanthony41@gmail.com>
2023-04-04 20:08:29 +0000

    #1 - Added a querybuilder class to initialize all classes

---
+++ query_builder/Querybuilder.js
@@ -0,0 +1,55 @@
+const SelectBuilder = require("./SelectBuilder");
+const InsertBuilder = require("./InsertBuilder");
+const CreateBuilder = require("./CreateBuilder");
+const FindByBuilder = require("./FindByBuilder");
+const UpdateBuilder = require("./UpdateBuilder");
+const RemoveBuilder = require("./RemoveBuilder");
+
+class QueryBuilder {
+  constructor() {
+    this.query = {};
+  }
+
+  select() {
+    this.selectBuilder = new SelectBuilder();
+    return this.selectBuilder;
+  }
+
+  insert(model) {
+    this.insertBuilder = new InsertBuilder(model);
+    return this.insertBuilder;
+  }
+
+  create(model) {
+    this.createBuilder = new CreateBuilder(model);
+    return this.createBuilder;
+  }
+
+  findBy(db){
+    this.findByBuilder = new FindByBuilder(db);
+    return this.findByBuilder;
+  }
+
+  update(model){
+    this.updateBuilder = new UpdateBuilder(model);
+    return this.updateBuilder;
+  }
+
+  remove(model){
+    this.removeBuilder = new RemoveBuilder(model);
+    return this.removeBuilder;
+  }
+
+  build() {
+    if (this.selectBuilder) {
+      return this.selectBuilder.build();
+    } else if (this.insertBuilder) {
+      return this.insertBuilder.build();
+    } else if (this.createBuilder) {
+      return this.createBuilder.build();
+    }
+  }
+}
+
+
+module.exports = QueryBuilder;
