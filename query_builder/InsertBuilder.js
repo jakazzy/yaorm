d355b1447b376d9de2c5055de26ee4f490860a54
Anthony4m <amoahanthony41@gmail.com>
2023-04-05 23:19:34 +0000

    #1 - refractored insertbuilder class to take a modelclass and the data to be inserted

--- query_builder/InsertBuilder.js
+++ query_builder/InsertBuilder.js
@@ -1,37 +1,57 @@
 class InsertBuilder {
-  constructor(tableName) {
-    this.tableName = tableName;
+  constructor(modelClass, data) {
+    this.modelClass = modelClass;
     this.columns = [];
     this.values = [];
-  }
-
-  column(columnName) {
-    this.columns.push(columnName);
-    return this;
-  }
-
-  values(rowValues) {
-    this.values.push(rowValues);
-    return this;
+    this.data = data;
   }
 
   build() {
-    const columns = this.columns.join(', ');
-    const values = this.values.map(rowValues =>
-      `(${rowValues.map(value => this.formatValue(value)).join(', ')})`
-    ).join(', ');
-    return `INSERT INTO ${this.tableName} (${columns}) VALUES ${values};`;
+    const tableName = `${this.modelClass.name.toLowerCase()}s`;
+    const columnNames = this.getColumnNames();
+    const values = this.getValues();
+
+    return `INSERT INTO ${tableName} (${columnNames}) VALUES (${values})`;
   }
 
-  formatValue(value) {
+  getColumnNames() {
+    const columnNames = Object.getOwnPropertyNames(new this.modelClass())
+      .filter((column) => column !== 'constructor');
+    return columnNames.join(', ');
+  }
+
+  getValues() {
+    const values = Object.getOwnPropertyNames(new this.modelClass())
+      .filter((column) => column !== 'constructor')
+      .map((column) => this.convertValue(this.data[column]));
+    return values.join(', ');
+  }
+
+  convertValue(value) {
     if (typeof value === 'string') {
       return `'${value}'`;
-    } else if (value instanceof Date) {
-      return `'${value.toISOString()}'`;
-    } else {
+    } else if (typeof value === 'number' || typeof value === 'boolean') {
       return value;
+    } else if (value instanceof Date) {
+      const year = value.getFullYear();
+      const month = value.getMonth() + 1;
+      const day = value.getDate();
+      return `'${year}-${month}-${day}'`;
+    } else if (typeof value === 'object' && value !== null) {
+      if (Array.isArray(value)) {
+        const arr = value.map(item => this.convertValue(item));
+        return `[${arr.join(', ')}]`;
+      } else {
+        const keys = Object.keys(value);
+        const columns = keys.map(key => `${key}`).join(', ');
+        const nestedValues = keys.map(key => this.convertValue(value[key])).join(', ');
+        return `(${columns}) VALUES (${nestedValues})`;
+      }
+    } else {
+      return null;
     }
   }
 }
+
 
 module.exports = InsertBuilder;
